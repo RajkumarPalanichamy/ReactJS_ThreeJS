b94afe670049564f1551c636a703ca72f92f81cf
rajkumarpalanichamy <rajempire2k20@gmail.com>
2024-12-30 03:56:00 +0530

    bin

--- bin/cli.js
+++ bin/cli.js
@@ -24,11 +24,15 @@ async function init() {
 
   const projectName = response.projectName;
   const gitCheckoutCommand = `git clone --depth 1 https://github.com/RajkumarPalanichamy/reactjs_threejs ${projectName}`;
+  const cleanupCommand = `cd ${projectName} && rm -rf .git bin node_modules package-lock.json`;
   const installDepsCommand = `cd ${projectName} && npm install`;
 
   console.log(chalk.blue(`Cloning the repository with name ${projectName}`));
   const checkedOut = runCommand(gitCheckoutCommand);
   if(!checkedOut) process.exit(-1);
+
+  const cleanedUp = runCommand(cleanupCommand);
+  if(!cleanedUp) process.exit(-1);
 
   console.log(chalk.blue(`Installing dependencies for ${projectName}`));
   const installedDeps = runCommand(installDepsCommand);
@@ -37,6 +41,7 @@ async function init() {
   console.log(chalk.green('\nSuccess! Created', projectName));
   console.log('\nProject is ready! You can run:');
   console.log(chalk.cyan('\n  cd'), projectName);
+  console.log(chalk.cyan('  npm install'));
   console.log(chalk.cyan('  npm start'));
   console.log('\nHappy coding!\n');
 }
