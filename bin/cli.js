875ab15d10262374c7341166503773587ed12031
rajkumarpalanichamy <rajempire2k20@gmail.com>
2024-12-30 12:00:54 +0530

    last update

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,9 @@
 #!/usr/bin/env node
 
-const { execSync } = require('child_process');
+const fs = require('fs-extra');
+const path = require('path');
 const prompts = require('prompts');
 const chalk = require('chalk');
-
-const runCommand = command => {
-  try {
-    execSync(`${command}`, { stdio: 'inherit' });
-  } catch (e) {
-    console.error(`Failed to execute ${command}`, e);
-    return false;
-  }
-  return true;
-}
 
 async function init() {
   const response = await prompts({
@@ -23,27 +14,37 @@ async function init() {
   });
 
   const projectName = response.projectName;
-  const gitCheckoutCommand = `git clone --depth 1 https://github.com/RajkumarPalanichamy/reactjs_threejs ${projectName}`;
-  const cleanupCommand = `cd ${projectName} && rm -rf .git bin node_modules package-lock.json`;
-  const installDepsCommand = `cd ${projectName} && npm install`;
+  const projectPath = path.resolve(process.cwd(), projectName);
 
-  console.log(chalk.blue(`Cloning the repository with name ${projectName}`));
-  const checkedOut = runCommand(gitCheckoutCommand);
-  if(!checkedOut) process.exit(-1);
+  console.log(chalk.blue(`Creating a new R3F app in ${projectPath}...`));
 
-  const cleanedUp = runCommand(cleanupCommand);
-  if(!cleanedUp) process.exit(-1);
+  // Create project directory
+  fs.mkdirSync(projectPath, { recursive: true });
 
-  console.log(chalk.blue(`Installing dependencies for ${projectName}`));
-  const installedDeps = runCommand(installDepsCommand);
-  if(!installedDeps) process.exit(-1);
+  // Copy template files
+  await fs.copy(path.join(__dirname, '../'), projectPath);
+
+
+  // Update package.json
+  const packageJson = require(path.join(projectPath, 'package.json'));
+  packageJson.name = projectName;
+  await fs.writeJSON(path.join(projectPath, 'package.json'), packageJson, { spaces: 2 });
 
   console.log(chalk.green('\nSuccess! Created', projectName));
-  console.log('\nProject is ready! You can run:');
+  console.log('\nInside that directory, you can run several commands:');
+  console.log(
+    chalk.cyan('\n  npm start'),
+    '\n    Starts the development server.'
+  );
+  console.log(
+    chalk.cyan('\n  npm run build'),
+    '\n    Bundles the app for production.'
+  );
+  console.log('\nWe suggest that you begin by typing:');
   console.log(chalk.cyan('\n  cd'), projectName);
   console.log(chalk.cyan('  npm install'));
   console.log(chalk.cyan('  npm start'));
-  console.log('\nHappy coding!\n');
+  console.log('\nHappy hacking!\n');
 }
 
 init().catch((err) => {
